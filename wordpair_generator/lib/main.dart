6b9fe69a4b553c568c7bb75ddc7cdf383a704577
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2022-04-30 12:22:07 +0800

    Update main.dart
    Use separate widget function to create home widget.

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,10 +8,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Wordpair Generator'),
-            ),
-            body: Center(child: Text('Hello World'))));
+        home: new RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final _randomWordPairs = <WordPair>[]
+
+    Widget _buildList() {
+      return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemBuilder: (context, int item) {
+          if (item.isOdd) return Divider();
+
+          final index = item ~/ 2;
+
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+        },
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Wordpair Generator'),
+        ),
+        body: _buildList());
   }
 }
