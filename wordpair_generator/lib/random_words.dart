5a96be86579c0ac2a6c2b046d72d33f9d68f7a02
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2022-04-30 13:45:00 +0800

    Update random_words.dart
    Added snackbar when saving a wordpair and appbar navigation to a page that shows the saved wordpairs.

--- wordpair_generator/lib/random_words.dart
+++ wordpair_generator/lib/random_words.dart
@@ -2,65 +2,93 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
   @override
   RandomWordsState createState() => RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
-    final _randomWordPairs = <WordPair>[];
-    final _savedWordPairs = Set<WordPair>();
+  final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
 
-    Widget _buildRow(WordPair pair) {
-      final alreadySaved = _savedWordPairs.contains(pair);
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
 
-      return ListTile(
-        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)),
-        trailing: Icon(
-          alreadySaved ? Icons.favorite : Icons.favorite_border,
-          color: alreadySaved ? Colors.red : null,
-        ),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _savedWordPairs.remove(pair);
-            } else {
-              _savedWordPairs.add(pair);
-            }
-          });
-        },
-      );
-    }
-
-    Widget _buildList() {
-      return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemBuilder: (context, int item) {
-          if (item.isOdd) return Divider();
-
-          final index = item ~/ 2;
-
-          if (index >= _randomWordPairs.length) {
-            _randomWordPairs.addAll(generateWordPairs().take(10));
+    return ListTile(
+      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            showSnackbar(pair.asPascalCase + " removed from saved wordpairs.");
+            _savedWordPairs.remove(pair);
+          } else {
+            showSnackbar(pair.asPascalCase + " added to saved wordpairs.");
+            _savedWordPairs.add(pair);
           }
+        });
+      },
+    );
+  }
 
-          return _buildRow(_randomWordPairs[index]);
-        },
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemBuilder: (context, int item) {
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  void showSnackbar(text) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+          title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)),
+        );
+      });
+
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+        appBar: AppBar(title: Text('Saved WordPairs')),
+        body: ListView(
+          children: divided,
+        ),
       );
-    }
+    }));
+  }
 
-    void _pushSaved() {
-
-    }
-
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('WordPair Generator'),
-          actions: <Widget>[IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved,
-          )],),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushSaved,
+            )
+          ],
+        ),
         body: _buildList());
   }
 }
