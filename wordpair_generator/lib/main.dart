29a6ec62109b63704c5a5eb6735760842ce5e46c
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2022-04-30 13:43:33 +0800

    Update main.dart

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -4,6 +4,8 @@ import 'random_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
